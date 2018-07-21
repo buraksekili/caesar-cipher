49c4caab0aad3bbbfa9f8d09bb2374f34f3de68f
buraksekili <buraksekili@gmail.com>
2018-07-21 12:40:43 +0300

    Kodun ingilizce olarak ilk versiyonu. Geliştirmeler yapılmadı!

---
+++ ceaser-cipher.py
@@ -0,0 +1,21 @@
+def ceaser(message, key):
+    encrypted_message = ''
+
+    alphabet = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    ]
+    for i in message:
+
+        if i not in alphabet:
+            encrypted_message += i
+        else:
+            encrypted_message += alphabet[
+                (alphabet.index(i)+shift) % len(alphabet)]
+
+    print("The encrypted message is:", encrypted_message)
+
+
+shift = int(input('Shift value: '))
+text = input("Your message: ")
+ceaser(text, shift)
